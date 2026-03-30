ee66c9b4b3d0340ba21a50882b108a5b51ff4e20
diego <sargent.pointed453@passinbox.com>
2026-03-30 12:40:15 +0200

    add: background, scrolling stars effect

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import random
+
 import pygame
 
 from ship import Ship
 from sprites import SpriteSheet
 
-
-animation_steps = 4
+cols = 13
 animation_cooldown = 100
 
 class Game:
@@ -17,22 +18,29 @@ class Game:
         self.running = True
         self.fps = 60
         self.frame = 0
-
+        self.scale = 3
         self.sprite_sheet = SpriteSheet("assets/ship.png")
+        framew = self.sprite_sheet.sheet.get_width() // cols
+        frameh = self.sprite_sheet.sheet.get_height()
         self.ship = Ship(self.sprite_sheet, 0, 0, self.frame,
                          self.sprite_sheet.sheet.get_width(),
                          self.sprite_sheet.sheet.get_height(), columns=4)
-        self.ship_x, self.ship_y = (self.screen_size[0]/2 -
-                                    self.ship.width/2, self.screen_size[1] - 200)
+        self.ship_x = self.screen_size[0] // 2 - framew // 2 - 25
+        self.ship_y = self.screen_size[1] // 2 + 200
 
-        self.cols = 8
+        self.frames_movement = []
         self.frames = []
-        framew = self.sprite_sheet.sheet.get_width() // self.cols
-        frameh = self.sprite_sheet.sheet.get_height()
-        for i in range(animation_steps):
-            img = self.sprite_sheet.get_image(i, framew, frameh, scale=2,
-                                              columns=self.cols)
+        for i in range(cols):
+            img = self.sprite_sheet.get_image(i, framew, frameh, scale=self.scale,columns=cols)
             self.frames.append(img)
+
+        self.frames_movement = self.frames[0:8]
+        self.frames_shooting = self.frames[8:12]
+        self.frame_idle = self.frames[12]
+
+        self.stars = [[random.randint(0, int(self.screen_size[0])),
+                       random.randint(0, int(self.screen_size[1])),
+                       random.randint(1, 3)] for _ in range(100)]
         self.last_update = pygame.time.get_ticks()
 
     def run(self):
@@ -40,28 +48,58 @@ class Game:
             self.clock.tick(self.fps)
             self.screen.fill((0,0,0))
 
+            for i in self.stars:
+                pygame.draw.circle(self.screen, (255, 255, 255),
+                                   (int(i[0]), int(i[1])), i[2])
+                i[1] -= 2
+                if i[1] < 0:
+                    (i[1]) = self.screen_size[1]
+                    i[0] = random.randint(0, int(self.screen_size[0]))
+                i[2] = random.randint(1, 3)
+
             current_time = pygame.time.get_ticks()
             if current_time - self.last_update >= animation_cooldown:
                 self.last_update = current_time
                 self.frame += 1
                 if self.frame >= len(self.frames):
                     self.frame = 0
-                self.ship.sprite = self.frames[self.frame]
+
+            if self.ship.state == "move":
+                img = self.frames_movement[
+                    self.frame % len(self.frames_movement)]
+            elif self.ship.state == "shoot":
+                img = self.frames_shooting[
+                    self.frame % len(self.frames_shooting)]
+            else:
+                img = self.frame_idle
+
+            self.screen.blit(img, [self.ship_x, self.ship_y])
+            key_pressed = pygame.key.get_pressed()
+
+            if key_pressed[pygame.K_LEFT] and self.ship_x > 0:
+                self.ship_x -= self.ship.velocity
+                self.ship.moving = True
+            if key_pressed[pygame.K_RIGHT] and self.ship_x < self.screen_size[0] - img.get_width():
+                self.ship_x += self.ship.velocity
+                self.ship.moving = True
+            if key_pressed[pygame.K_UP] and self.ship_y > 0:
+                self.ship_y -= self.ship.velocity
+                self.ship.moving = True
+            if key_pressed[pygame.K_DOWN] and self.ship_y < self.screen_size[1] - self.ship.sprite.get_height():
+                self.ship_y += self.ship.velocity
+                self.ship.moving = False
+
+            if key_pressed[pygame.K_SPACE]:
+                self.ship.state = "shoot"
+            elif self.ship.moving:
+                self.ship.state = "move"
+            else:
+                self.ship.state = "idle"
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            self.screen.blit(self.ship.sprite, [self.ship_x, self.ship_y])
-            key_pressed = pygame.key.get_pressed()
-            if key_pressed[pygame.K_LEFT] and self.ship_x > 0:
-                self.ship_x -= self.ship.velocity
-            if key_pressed[pygame.K_RIGHT] and self.ship_x < self.screen_size[0] - self.ship.sprite.get_width():
-                self.ship_x += self.ship.velocity
-            if key_pressed[pygame.K_UP] and self.ship_y > 0:
-                self.ship_y -= self.ship.velocity
-            if key_pressed[pygame.K_DOWN] and self.ship_y < self.screen_size[1] - self.ship.sprite.get_height():
-                self.ship_y += self.ship.velocity
             pygame.display.update()
         pygame.quit()
 
