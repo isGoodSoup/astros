e869ed2a0b9ee61a22189239316318d38375baec
diego <sargent.pointed453@passinbox.com>
2026-03-31 14:39:55 +0200

    update: moved scripts away to /scripts/

--- scripts/asteroid.py
+++ scripts/asteroid.py
@@ -7,7 +7,7 @@ class Asteroid(pygame.sprite.Sprite):
     def __init__(self, screen_width, min_y=-150, max_y=0, scale=2, speed=8):
         super().__init__()
         self.asteroids = [1,2,3,4,5,7,8]
-        self.image = pygame.image.load(f"assets/asteroids/asteroid"
+        self.image = pygame.image.load(f"../assets/asteroids/asteroid"
                                        f"_{random.choice(self.asteroids)}.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, [self.image.get_width() * scale,
                                                          self.image.get_height() * scale])
