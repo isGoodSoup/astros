680d07633250db106d7aa479d182f1ff8996687c
diego <sargent.pointed453@passinbox.com>
2026-03-31 13:12:46 +0200

    add: full rework, last one, of the ship

--- ship.py
+++ ship.py
@@ -1,16 +1,18 @@
 import pygame
 
 class Ship(pygame.sprite.Sprite):
-    def __init__(self, sprite_sheet, x, y, frame, width, height, scale=3, columns=1):
+    def __init__(self, sprite_sheet, x, y, frame, width, height, scale=4,
+                 columns=1):
         super().__init__()
         self.image = sprite_sheet.get_image(frame, width, height, scale, columns)
         self.rect = self.image.get_rect(topleft=(x, y))
-        self.hitbox = self.rect.inflate(self.rect.width * -0.4,
-                                        self.rect.height * -0.4)
+        self.hitbox = self.rect.inflate(self.rect.width * -0.6,
+                                        self.rect.height * -0.6)
         self.velocity = 8
-        self.state = "move"
+        self.direction = "idle"
+        self.shooting = False
         self.moving = False
 
     def update_position(self, x, y):
         self.rect.topleft = (x, y)
-        self.hitbox.center = self.rect.center
+        self.hitbox.topleft = self.rect.topleft
