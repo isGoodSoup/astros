f9bbed5b791908e74bcebef00fcc4e8ac3c2f625
diego <sargent.pointed453@passinbox.com>
2026-04-01 17:32:25 +0200

    update: reworked Interface to be generic and shield sprite

--- scripts/hud.py
+++ scripts/hud.py
@@ -2,19 +2,19 @@ import pygame
 from scripts.sheet import SpriteSheet
 
 class Interface(pygame.sprite.Sprite):
-    def __init__(self, frame, width, height, hud_ratio, scale=4,
-                 columns=29):
+    def __init__(self, path, frame, width, height, hud_ratio, columns=29,
+                 offset=(0, 0), scale=4):
         super().__init__()
-        self.sprite_sheet = SpriteSheet("assets/ui/status.png")
+        self.sprite_sheet = SpriteSheet(path)
         self.frames = [self.sprite_sheet.get_image(i, width, height, scale, columns)
             for i in range(columns)]
         self.image = self.frames[frame]
-
         self.hud_x = hud_ratio['right'] - self.image.get_width()
         self.hud_y = hud_ratio['bottom'] - self.image.get_height()
+        self.offset_x, self.offset_y = offset
 
     def update(self, ship, hud_ratio, frame, screen):
         frame = min(frame, len(self.frames) - 1)
-        self.hud_x = hud_ratio['right'] - self.image.get_width()
-        self.hud_y = hud_ratio['bottom'] - self.image.get_height()
+        self.hud_x = hud_ratio['right'] - self.image.get_width() + self.offset_x
+        self.hud_y = hud_ratio['bottom'] - self.image.get_height() + self.offset_y
         screen.blit(self.frames[frame], (self.hud_x, self.hud_y))
