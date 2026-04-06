fc3cda5df3c7ab76b8ff84cb53ab242b80b3d317
diego <sargent.pointed453@passinbox.com>
2026-04-06 15:57:05 +0200

    update: formatted credits score

--- scripts/credits.py
+++ scripts/credits.py
@@ -1,4 +1,15 @@
 
 def update_credits(game, game_font):
-    formatted_credits = f"{game.ship.credits:,.0f}".replace(',', '.') + "€$"
+    short_credits = format_number(game.ship.credits)
+    formatted_credits = f"{short_credits}€$"
     game.credits = game_font.render(formatted_credits, True, (255, 200, 0))
+
+def format_number(n):
+    if n >= 1_000_000_000:
+        return f"{n / 1_000_000_000:.2f}B"
+    elif n >= 1_000_000:
+        return f"{n / 1_000_000:.2f}M"
+    elif n >= 1_000:
+        return f"{n / 1_000:.2f}K"
+    else:
+        return f"{n:.0f}"
