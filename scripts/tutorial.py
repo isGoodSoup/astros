19bbf1aae6e1985d6cb3aedf54484b356dfc576d
diego <sargent.pointed453@passinbox.com>
2026-04-10 19:42:23 +0200

    add: WASD movement keys for ship and updated tutorial

--- scripts/tutorial.py
+++ scripts/tutorial.py
@@ -1,5 +1,7 @@
 import pygame
 
+from scripts.settings import COLOR_WHITE
+
 
 class Tutorial:
     def __init__(self):
@@ -32,21 +34,21 @@ class Tutorial:
 
     def render(self, screen, font):
         if self.step == 0:
-            text = font.render("Move your ship", True, (255, 255, 255))
+            text = font.render("Move your ship WASD or Arrow Keys", True, COLOR_WHITE)
             screen.blit(text, (pygame.display.Info().current_w // 2 - text.get_width() // 2,
                                pygame.display.Info().current_h - 100))
 
         elif self.step == 1:
-            text = font.render("Press SPACE / A to shoot", True,(255, 255, 255))
+            text = font.render("Press SPACE/A to shoot", True,COLOR_WHITE)
             screen.blit(text, (pygame.display.Info().current_w // 2 - text.get_width() // 2,
                                pygame.display.Info().current_h - 100))
 
         elif self.step == 2:
-            text = font.render("Press G / X to switch gun mode", True, (255, 255, 255))
+            text = font.render("Press G/X to switch gun mode", True, COLOR_WHITE)
             screen.blit(text, (pygame.display.Info().current_w // 2 - text.get_width() // 2,
                                pygame.display.Info().current_h - 100))
 
         elif self.step == 3:
-            text = font.render("Survive.", True,(255, 255, 255))
+            text = font.render("Survive...", True,COLOR_WHITE)
             screen.blit(text, (pygame.display.Info().current_w // 2 - text.get_width() // 2,
                                pygame.display.Info().current_h - 100))
