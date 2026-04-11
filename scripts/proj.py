da29247e4ce3168c750685b7c3de690363987f67
diego <sargent.pointed453@passinbox.com>
2026-04-11 17:13:41 +0200

    update: homing circle projectiles 2

--- scripts/proj.py
+++ scripts/proj.py
@@ -1,6 +1,9 @@
 import pygame
 import math
 
+from scripts.settings import COLOR_LIGHT_RED
+
+
 class Projectile(pygame.sprite.Sprite):
     def __init__(self, pos, color, direction=(0, -1), speed=16,
                  damage=1, range_limit=900,
@@ -8,12 +11,9 @@ class Projectile(pygame.sprite.Sprite):
                  explosive=False, explosion_radius=0):
         super().__init__()
         self.screen = screen
-        self.image = pygame.Surface([4, 20], pygame.SRCALPHA)
-        self.image.fill((*color, 255))
-        self.rect = self.image.get_rect(center=pos)
         self.speed = speed
         self.damage = damage
-        self.direction = direction
+        self.direction = pygame.Vector2(direction).normalize()
         self.range_limit = range_limit
         self.distance_traveled = 0
 
@@ -23,30 +23,36 @@ class Projectile(pygame.sprite.Sprite):
         self.explosion_radius = explosion_radius
         self.nuke = False
 
-        angle = -math.degrees(math.atan2(direction[1], direction[0])) - 90
-        self.image = pygame.transform.rotate(self.image, angle)
+        if self.homing:
+            radius = 6
+            proj_color = COLOR_LIGHT_RED
+            self.image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
+            pygame.draw.circle(self.image, proj_color, (radius, radius), radius)
+        else:
+            self.image = pygame.Surface((4, 20), pygame.SRCALPHA)
+            self.image.fill((*color, 255))
+
+            angle = -math.degrees(math.atan2(self.direction.y, self.direction.x)) - 90
+            self.image = pygame.transform.rotate(self.image, angle)
+        self.rect = self.image.get_rect(center=pos)
 
     def update(self, delta_time=1.0):
-        if self.homing and self.target:
-            self.image = pygame.draw.circle(self.screen, pygame.SRCALPHA, self.image.get_rect(
-                                                center=self.target), 20)
+        if self.homing and self.target and self.target.alive():
             tx, ty = self.target.rect.center
-            dx = tx - self.rect.centerx
-            dy = ty - self.rect.centery
+            target_vec = pygame.Vector2(tx - self.rect.centerx,
+                                        ty - self.rect.centery)
 
-            vec = pygame.Vector2(dx, dy)
-            if vec.length() > 0:
-                vec = vec.normalize()
-
+            if target_vec.length() > 0:
+                target_vec = target_vec.normalize()
                 current = pygame.Vector2(self.direction)
-                new_dir = current.lerp(vec, 0.08)
-                self.direction = (new_dir.x, new_dir.y)
+                new_dir = current.lerp(target_vec, 0.08)
+                self.direction = new_dir.normalize()
 
-        dx = self.direction[0] * self.speed * delta_time
-        dy = self.direction[1] * self.speed * delta_time
+        dx = self.direction.x * self.speed * delta_time
+        dy = self.direction.y * self.speed * delta_time
         self.rect.x += dx
         self.rect.y += dy
-        self.distance_traveled += math.sqrt(dx*dx + dy*dy)
+        self.distance_traveled += math.hypot(dx, dy)
 
         if self.distance_traveled >= self.range_limit:
             self.kill()
