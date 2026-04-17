f76b70d1edef6d9d025f6f25fb78e14da522867e
diego <sargent.pointed453@passinbox.com>
2026-04-18 00:00:02 +0200

    fix: crt data mismatch bug 2

--- scripts/engine/crt.py
+++ scripts/engine/crt.py
@@ -2,6 +2,7 @@ import struct
 import pygame, os, sys
 import moderngl
 
+
 def resource_path(relative):
     if hasattr(sys, "_MEIPASS"):
         absolute_path = os.path.join(sys._MEIPASS, relative)
@@ -9,8 +10,10 @@ def resource_path(relative):
         absolute_path = os.path.join(relative)
     return absolute_path
 
+
 class CRT:
-    def __init__(self, screen, style=1, virtual_resolution=(800, 600), cpu_only=False,
+    def __init__(self, screen, style=1, virtual_resolution=(800, 600),
+                 cpu_only=False,
                  fullscreen=False):
         pygame.init()
         self.virtual_resolution = virtual_resolution
@@ -26,8 +29,11 @@ class CRT:
             self.render_indices = [0, 1, 2,
                                    1, 2, 3]
             self.style = style
-            self.prog = self.ctx.program(vertex_shader=open(resource_path('assets/shaders/VERTEX_SHADER.glsl')).read(),
-                fragment_shader=open(resource_path('assets/shaders/FRAGMENT_SHADER.glsl')).read())
+            self.prog = self.ctx.program(
+                vertex_shader=open(
+                resource_path('assets/shaders/VERTEX_SHADER.glsl')).read(),
+                fragment_shader=open(resource_path(
+                'assets/shaders/FRAGMENT_SHADER.glsl')).read())
             self.prog['mode'] = self.style
 
             self.screen_texture = self.ctx.texture(self.virtual_resolution, 4)
@@ -52,12 +58,15 @@ class CRT:
 
     def change_shader(self):
         if not self.cpu_only:
-            self.__init__(self.screen, (self.style + 1) % 3, self.virtual_resolution)
+            self.__init__(self.screen, (self.style + 1) % 3,
+                          self.virtual_resolution)
 
     def render(self, surface):
         if not self.cpu_only:
+            surface = pygame.transform.scale(surface, self.virtual_resolution)
+            surface = surface.convert_alpha()
             texture_data = pygame.image.tostring(surface, "RGBA", False)
-            self.screen_texture.write(texture_data)
+            self.screen_texture.write(texture_data, alignment=1)
             self.ctx.clear(14 / 255, 40 / 255, 66 / 255)
             self.screen_texture.use()
             self.vao.render()
@@ -69,7 +78,8 @@ class CRT:
     def fullscreen(self, real_resolution):
         if not self.cpu_only:
             if not self.fullscreen:
-                pygame.display.set_mode(real_resolution, pygame.DOUBLEBUF | pygame.OPENGL)
+                pygame.display.set_mode(real_resolution,
+                                        pygame.DOUBLEBUF | pygame.OPENGL)
             else:
                 pygame.display.set_mode(real_resolution, pygame.DOUBLEBUF |
                                         pygame.OPENGL | pygame.FULLSCREEN)
@@ -77,7 +87,8 @@ class CRT:
             if not self.fullscreen:
                 pygame.display.set_mode(self.virtual_resolution)
             else:
-                pygame.display.set_mode(self.virtual_resolution, pygame.FULLSCREEN)
+                pygame.display.set_mode(self.virtual_resolution,
+                                        pygame.FULLSCREEN)
 
     def __call__(self, surface):
         return self.render(surface)
